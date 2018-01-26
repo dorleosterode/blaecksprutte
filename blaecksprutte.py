eaaa826ca7efb9a0d53968e899caa733e54950a6
Dorle Osterode <9osterod@informatik.uni-hamburg.de>
2018-01-26 12:33:27 +0100

    prototyp for learning spam-filter working on notmuch databases

---
+++ blaecksprutte.py
@@ -0,0 +1,79 @@
+import argparse
+import cPickle
+from sklearn.linear_model import SGDClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import label_ranking_average_precision_score
+from sklearn.metrics import label_ranking_loss
+
+import extract_mails
+
+def validate():
+    print "getting data"
+    data, labels = extract_mails.get_training_data()
+    print "splitting data"
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.4, random_state=0)
+
+    print "preprocess data"
+    vectorizer = CountVectorizer()
+    vectorizer.fit(data)
+    X = vectorizer.transform(x_train)
+    binarizer = MultiLabelBinarizer()
+    binarizer.fit(labels)
+    Y = binarizer.transform(y_train)
+
+    print "train classifier"
+    clf = OneVsRestClassifier(SGDClassifier())
+    clf.fit(X, Y)
+
+    print "evaluate classifier"
+    Xt = vectorizer.transform(x_test)
+    preds = clf.predict(Xt)
+    real = binarizer.transform(y_test)
+
+    print "average precision:"
+    print label_ranking_average_precision_score(real, preds)
+    print "ranking loss:"
+    print label_ranking_loss(real, preds)
+
+def train_from_bottom():
+    train_data, train_labels = extract_mails.get_training_data()
+
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(train_data)
+    binarizer = MultiLabelBinarizer()
+    Y = binarizer.fit_transform(train_labels)
+
+    clf = OneVsRestClassifier(SGDClassifier())
+    clf.fit(X, Y)
+
+    return vectorizer, binarizer, clf
+
+def tag_new_mails(v, b, c):
+    data, ids = extract_mails.get_new_mails()
+    X = v.transform(data)
+    preds = c.predict(X)
+    tags = b.inverse_transform(preds)
+    extract_mails.write_tags(ids, tags)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train", help="train the tagger from standard notmuch database", action="store_true")
+    parser.add_argument("--tag", help="tag the mails with a new-tag", action="store_true")
+    args = parser.parse_args()
+
+    filename = "tagger.pkl"
+
+    if args.tag:
+        with open(filename, 'rb') as f:
+            v, b, c = cPickle.load(f)
+        tag_new_mails(v, b, c)
+    elif args.train:
+        v, b, c = train_from_bottom()
+        with open(filename, 'wb') as f:
+            cPickle.dump([v, b, c], f)
+
+if __name__ == "__main__":
+    main()
---
+++ extract_mails.py
@@ -0,0 +1,56 @@
+import sys
+from notmuch import Database, Query
+
+def erase_irrelevant_tags(lst):
+    irrelevant = {"attachment", "draft", "encrypted", "new", "signed", "unread", "inbox"}
+    return filter(lambda x: x not in irrelevant, lst)
+
+def get_training_data():
+    training_data = []
+    training_labels = []
+    db = Database()
+    # query that returns all the messages
+    q = Query(db, '')
+    data = []
+    for m in q.search_messages():
+	data.append(m.get_header('To'))
+	data.append(m.get_header('From'))
+	data.append(m.get_header('Subject'))
+	data.append(m.get_part(1).decode("utf8", errors="ignore"))
+	try:
+	    training_data.append('\n'.join(data))
+	except UnicodeDecodeError:
+	    print map(lambda x: type(x), data)
+	    sys.exit(1)
+	training_labels.append(erase_irrelevant_tags(list(m.get_tags())))
+	data = []
+
+    return training_data, training_labels
+
+def get_new_mails():
+    db = Database()
+    query = Query(db, 'tag:new')
+    data = []
+    ids = []
+    m_data = []
+    for m in query.search_messages():
+        m_data.append(m.get_header('To'))
+	m_data.append(m.get_header('From'))
+	m_data.append(m.get_header('Subject'))
+	m_data.append(m.get_part(1).decode("utf8", errors="ignore"))
+	try:
+	    data.append('\n'.join(m_data))
+            ids.append(m.get_message_id())
+	except UnicodeDecodeError:
+	    print map(lambda x: type(x), m_data)
+	    sys.exit(1)
+        m_data = []
+    return data, ids
+
+def write_tags(ids, tags):
+    db = Database(mode=Database.MODE.READ_WRITE)
+    for i, ts in zip(ids, tags):
+        m = db.find_message(i)
+        m.remove_tag("new")
+        for t in ts:
+            m.add_tag(t)
